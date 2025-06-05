d6f09723a8d5f56c12b35519ec3cf7f19eeca5d7
vladcop1327 <vladcop30@gmail.com>
2025-06-05 20:13:32 +0300

    браузе5

--- photo_stitcher_bot.py
+++ photo_stitcher_bot.py
@@ -137,4 +137,8 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
